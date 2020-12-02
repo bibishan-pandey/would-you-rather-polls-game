7e74b99dc027771d5adec52e794f98eb1e1c27c8
Bibishan Pandey <bibishan.b.pandey@gmail.com>
2020-12-02 18:29:40 +0545

    feat: converted App component to connected component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Switch, Redirect, Route } from "react-router-dom";
+import {connect} from 'react-redux';
 
 import {
   ERROR_404,
@@ -10,32 +11,46 @@ import {
   SIGN_IN,
 } from "./settings/urls";
 
+import AuthenticatedRoute from "./components/AuthenticatedRoute";
 import NavBar from "./components/Navbar";
 
+import {handleGetUsers} from "./actions/users";
+import {handleGetQuestions} from "./actions/questions";
+
 class App extends Component {
+  componentDidMount() {
+    const {dispatch, questions, users} = this.props;
+    dispatch(handleGetUsers());
+    dispatch(handleGetQuestions());
+  }
+
   render() {
+    const {isAuthenticated} = this.props;
+
     return (
       <div>
         <NavBar />
         <div className={"container-fluid"}>
           <Switch>
+
             {/*TODO: Add Home/Dashboard Component*/}
-            {/*<Route path={HOME_URL} exact component={DashboardContainer} />*/}
+            {/*<AuthenticatedRoute exact path={HOME_URL} component={Dashboard} isAuthenticated={isAuthenticated}/>*/}
 
             {/*TODO: Add New Questions Component*/}
-            {/*<Route path={NEW_QUESTIONS} component={NewQuestionsContainer} />*/}
+            {/*<AuthenticatedRoute path={NEW_QUESTIONS} component={AddQuestion} isAuthenticated={isAuthenticated}/>*/}
 
             {/*TODO: Add Detail of Question/Poll*/}
-            {/*<Route path={QUESTIONS + '/:question_id'} component={QuestionsContainer} />*/}
+            {/*<AuthenticatedRoute path={QUESTIONS + '/:question_id'} component={Questions} isAuthenticated={isAuthenticated}/>*/}
 
             {/*TODO: Add Leaderboard container*/}
-            {/*<Route path={LEADERBOARD} component={LeaderboardContainer} />*/}
+            {/*<AuthenticatedRoute path={LEADERBOARD} component={Leaderboard} isAuthenticated={isAuthenticated}/>*/}
 
             {/*TODO: Add sign in container*/}
-            {/*<Route path={SIGN_IN} component={SignInContainer} />*/}
+            {/*<Route path={SIGN_IN} component={SignIn}/>*/}
 
             {/*TODO: Add 404 component*/}
-            {/*<Route path={ERROR_404} component={Error_404Container} />*/}
+            {/*<Route path={ERROR_404} component={Error404}/>*/}
+
             <Redirect to={ERROR_404} />
           </Switch>
         </div>
@@ -44,4 +59,12 @@ class App extends Component {
   }
 }
 
-export default App;
+function mapStateToProps({authUser, questions, users}) {
+  return {
+    isAuthenticated: !!authUser,
+    questions,
+    users
+  };
+}
+
+export default connect(mapStateToProps)(App);
