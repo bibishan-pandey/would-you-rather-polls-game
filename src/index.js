24e47999822289a06173e93b1f5978bd7e2b1c54
Bibishan Pandey <bibishan.b.pandey@gmail.com>
2020-12-02 18:08:08 +0545

    feat: added store to index.js with Provider

--- src/index.js
+++ src/index.js
@@ -1,19 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 
 import reportWebVitals from "./reportWebVitals";
 
+import middleware from "./middleware";
+import reducer from "./reducers";
+
 import App from "./App";
+
+const store = createStore(reducer, composeWithDevTools(middleware));
 
 const Root = () => {
   return (
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   );
 };
 
